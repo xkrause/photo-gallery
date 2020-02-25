bcf340e394825747c55c8ff8b5a58b69c0b15031
Xander Krause <sk8rak@gmail.com>
2020-02-25 13:56:09 -0800

    Basic CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <div className = "jumbotron">
-                <h1>Photo Gallery</h1>
+                <h1 className = "title">Photo Gallery</h1>
             </div>
         );
     }
