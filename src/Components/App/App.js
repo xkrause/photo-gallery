d6d8663c418230d1b998d7a337d60b38a6109023
Xander Krause <sk8rak@gmail.com>
2020-02-25 13:52:36 -0800

    Creating Header Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            <Header />
         );
     }
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className = "jumbotron">
+                <h1>Photo Gallery</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
