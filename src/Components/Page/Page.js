0af4d7157041f4e28fb49e909388c307d774154c
Xander Krause <sk8rak@gmail.com>
2020-02-26 17:24:35 -0800

    New images and scaling

--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -3,20 +3,25 @@ import Gallery from 'react-photo-gallery';
 
 const photos = [
     {
-      src: require(''),
-      width: 4,
-      height: 3
+        src: require('../../img/rocks.jpg'),
+        width: 6,
+        height: 7
     },
     {
-      src: require(''),
-      width: 1,
-      height: 1
+        src: require('../../img/apple.jpg'),
+        width: 4,
+        height: 3
     },
     {
-        src: require(''),
-        width: 1,
-        height: 1
-      }
+        src: require('../../img/rocks-close.jpg'),
+        width: 6,
+        height: 7
+    },
+    {
+        src: require('../../img/food.jpg'),
+        width: 4,
+        height: 4
+    }
   ];
 
 class Page extends React.Component {
